eb7f3d4a13ef362a3a7195f86fe01f485e210cdd
DEEPAK MUDULI <deepakmuduli272@gmail.com>
2026-01-18 15:48:53 +0530

    This is technical class program

---
+++ technical program/evenodd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num,i;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    if(num/2==0){
+        printf("%d is even number.",num);
+    }
+    else{
+        printf("%d is odd number.",num);
+    }
+    return 0;
+}
