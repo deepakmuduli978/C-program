ea78114588e40b5de03675c1177e24504d249bd1
DEEPAK MUDULI <deepakmuduli272@gmail.com>
2026-01-20 06:57:09 +0530

    day 2 fibonacci series program

---
+++ technical program/fibonacci series.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int first=0,second=1;
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    int i,next;
+    for(i=0;i<n;i++){
+        printf("%d ",first);
+        next=first+second;
+        first=second;
+        second=next;
+    }
+    return 0;
+}
